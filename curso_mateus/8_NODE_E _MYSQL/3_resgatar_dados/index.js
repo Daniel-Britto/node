f9b05634812e498c41028e59e721c16edcaab48c
Carlos Daniel Brito <92636711+Daniel-Britto@users.noreply.github.com>
2024-02-13 20:14:25 -0300

    dia 13.02.2024

---
+++ curso_mateus/8_NODE_E _MYSQL/3_resgatar_dados/index.js
@@ -0,0 +1,74 @@
+const express = require('express')
+const exphbs = require('express-handlebars')
+const mysql = require('mysql')
+
+const app = express()
+
+app.use(
+  express.urlencoded({
+    extended: true
+  })
+)
+
+app.use(express.json())
+
+app.engine('handlebars', exphbs())
+app.set('view engine', 'handlebars')
+
+app.use(express.static('public'))
+
+app.get('/', function (req, res) {
+  res.render('home')
+})
+
+app.post('/books/insertbook', (req, res) => {
+
+  const title = req.body.title
+  const pageqty = req.body.pagesqty
+
+  const sql = `INSERT INTO books (title, pagesqty) VALUES ('${title}', '${pageqty}')`
+
+  conn.query(sql, function(err) {
+    if(err) {
+      console.log(err)
+      return
+    }
+
+    res.redirect('/books')
+  })
+})
+
+app.get('/books', (req, res) => {
+  const sql = "SELECT * FROM books"
+
+  conn.query(sql, function(err, data) {
+     
+    if(err) {
+      console.log(err)
+      return
+    }
+
+    const books = data
+
+    console.log(books)
+
+    res.render('books', { books })
+  })
+})
+
+const conn = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'kbrito2913',
+  database: 'nodemysql',
+})
+
+conn.connect(function (err) {
+  if (err) {
+    console.log(err)
+  }
+
+  console.log('Conectado ao MySQL!')
+
+  app.listen(3000)
+})
---
+++ curso_mateus/8_NODE_E _MYSQL/9_connection_poll/index.js
@@ -0,0 +1,132 @@
+const express = require('express')
+const exphbs = require('express-handlebars')
+const pool = require('./db/conn')
+
+const app = express()
+
+app.use(
+  express.urlencoded({
+    extended: true
+  })
+)
+
+app.use(express.json())
+
+app.engine('handlebars', exphbs())
+app.set('view engine', 'handlebars')
+
+app.use(express.static('public'))
+
+app.get('/', function (req, res) {
+  res.render('home')
+})
+
+app.post('/books/insertbook', (req, res) => {
+
+  const title = req.body.title
+  const pageqty = req.body.pagesqty
+
+  const sql = `INSERT INTO books (??, ??) VALUES (?, ?)`
+  const data = ['title', 'pagesqty', title, pageqty]
+
+  pool.query(sql, data, function(err) {
+    if(err) {
+      console.log(err)
+      return
+    }
+
+    res.redirect('/books')
+  })
+})
+
+app.get('/books', (req, res) => {
+  const sql = "SELECT * FROM books"
+
+  pool.query(sql, function(err, data) {
+     
+    if(err) {
+      console.log(err)
+      return
+    }
+
+    const books = data
+
+    console.log(books)
+
+    res.render('books', { books })
+  })
+})
+
+//Rota para selecionar dado
+app.get('/books/:id', (req, res) => {
+  const id = req.params.id
+
+  const sql = `SELECT * FROM books WHEHE ?? = ?`
+  const data = ['id', id]
+
+  pool.query(sql, data, function(err, data) {
+    if(err) {
+      console.log(err)
+      return
+    }
+
+    const book = data[0]
+
+    res.render('book', { book })
+  })
+})
+
+// Rota para selecionar dado específico
+app.get('/books/edit/:id', (req, res) => {
+  const id = req.params.id
+  
+  const sql = `SELECT * FROM books WHERE id = ${id}`
+
+  pool.query(sql, function(err, data) {
+    if(err){
+      console.log(err)
+      return
+    }
+
+    const book = data[0]
+
+    res.render('editbook', { book })
+  })
+})
+
+// Rota para atualizar dados
+app.post('/books/updatebook', (req, res) => {
+  const id = req.body.id
+  const title = req.body.title
+  const pagesqty = req.body.pagesqty
+
+  const sql = `UPDATE books SET title = '${title}', pagesqty = '${pagesqty}' WHERE id = ${id}`
+
+  pool.query(sql, function(err) {
+    if(err) {
+      console.log(err)
+      return
+    }
+
+    res.redirect('/books')
+  })
+})
+
+// Rota para deletar dados
+app.post('/books/remove/:id', (req, res) => {
+
+  const id = req.params.id
+
+  const sql = `DELETE FROM books WHERE id = ${id}`
+
+  pool.query(sql, function(err) {
+    if(err) {
+      console.log(err)
+      return
+    }
+
+    res.redirect('/books')
+  })
+})
+
+app.listen(3000)
