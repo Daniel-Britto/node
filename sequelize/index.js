dde46fc1139339810156ba0661799afa15d9aed9
Carlos Daniel Brito <92636711+Daniel-Britto@users.noreply.github.com>
2024-03-27 18:39:00 -0300

    27/03/2024

--- sequelize/index.js
+++ sequelize/index.js
@@ -19,9 +19,10 @@ app.use(
     })
 )
 
-app.get('/', (req, res) => {
+app.get('/product', (req, res) => {
     res.render('product')
 })
+
 
 // ADICIONANDO DADOS AO BANCO
 app.post('/product/create', async (req, res) => {
@@ -35,6 +36,48 @@ app.post('/product/create', async (req, res) => {
 
     res.redirect('/')
 })
+
+// RESGATANDO DADOS DO BANCO
+app.get('/', async (req, res) => {
+
+    // métodos finders são aqueles que geram consulta(SELECT)
+    // raw: true: serve para desabilitar  o empacotamento e receber uma resposta simples
+    // findAll: método que recupera todas as entradas da tabela
+    const products = await Product.findAll({ raw: true })
+
+    res.render('allproducts', { products })
+})
+
+// DELETANDO DADOS DO BANCO
+app.post('/product/remove/:id', async(req, res) => {
+
+    // resgatar o id do produto
+    const id = req.params.id
+
+    // destroy(): utilizado para deletar mutiplas instâncias
+    // where: opção utilizada para localizar a entrada
+    await Product.destroy({ where: { id: id } })
+
+    res.redirect('/')
+
+})
+
+// SELECIONANDO DADO ESPECÍFICO
+app.get('/product/:id', async (req, res) => {
+    const id = req.params.id
+
+    try {
+        // findOne(): método usado para resgatar um dado específico
+        const product = await Product.findOne({ where: { id:id }})
+
+        // renderizamos a nossa viewProduct e mandamos a nossa variável
+        res.render('viewProduct', { product: product.get({ plain: true }) })
+
+    }catch(error) {
+        console.log(error)
+    }
+})
+
 
 
 conn.sync()
